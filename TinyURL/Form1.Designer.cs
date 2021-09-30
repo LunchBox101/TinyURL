eaa702c7016a8c8a83b3060ea45ab06d564e3b9d
Lancetl <lancetleonard@hotmail.com>
2021-09-30 14:11:39 -0400

    Add project files.

---
+++ TinyURL/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace TinyURL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.longURLTextBox = new System.Windows.Forms.TextBox();
+            this.tinyURLTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // longURLTextBox
+            // 
+            this.longURLTextBox.Location = new System.Drawing.Point(12, 26);
+            this.longURLTextBox.Name = "longURLTextBox";
+            this.longURLTextBox.Size = new System.Drawing.Size(208, 20);
+            this.longURLTextBox.TabIndex = 0;
+            // 
+            // tinyURLTextBox
+            // 
+            this.tinyURLTextBox.Location = new System.Drawing.Point(12, 105);
+            this.tinyURLTextBox.Name = "tinyURLTextBox";
+            this.tinyURLTextBox.Size = new System.Drawing.Size(208, 20);
+            this.tinyURLTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 165);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tinyURLTextBox);
+            this.Controls.Add(this.longURLTextBox);
+            this.Name = "Form1";
+            this.Text = "TinyURL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox longURLTextBox;
+        private System.Windows.Forms.TextBox tinyURLTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TinyURL/ShrinkURL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TinyURL
+{
+    public static class ShrinkURL
+    {
+        public static string GetShrinkURL(string strURL)
+        {
+            string URL;
+            URL = "http://tinyurl.com/api-create.php?url=" + strURL.ToLower();
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(URL);
+                request.Method = "Get";
+                using (HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(httpWebResponse.GetResponseStream()))
+                    {
+                        strURL = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            return strURL;
+        }
+    }
+}
